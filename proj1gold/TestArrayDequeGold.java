ec9310a8496e48f15f54eb8ca02966ed00ba5817
Yu-Hsuan-Lin <anitalin0105@gmail.com>
2021-11-09 17:41:35 +0800

    complete proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,85 +6,39 @@ public class TestArrayDequeGold {
     static StudentArrayDeque<Integer> stuA = new StudentArrayDeque<>();
     static ArrayDequeSolution<Integer> arrD = new ArrayDequeSolution<>();
 
+
     @Test
-    public void testWithMsg() {
-        StudentArrayDeque stu = new StudentArrayDeque<Integer>();
-        ArrayDequeSolution sol = new ArrayDequeSolution<Integer>();
-
-        for (int i = 0; i <= 10; i++) {
-            int last = StdRandom.uniform(11, 20);
-
-            stu.addLast(last);
-            sol.addLast(last);
-
-        }
-
-        String msg = helper1(sol);
-        for (int i = 0; i <= 10; i++) {
-
-            Object expected = sol.removeLast();
-            Object actual = stu.removeLast();
-
-            assertEquals(helper(i, msg), expected, actual);
-        }
-
-
-    }
-
-    private String helper1(ArrayDequeSolution<Integer> ads) {
-        String newMsg;
-        String res = "Oh noooo!\n   This is bad:\n   Related Operation:\n";
-
-        for(int i = 0; i<10; i++) {
-            newMsg = "   addLast(" + ads.get(i) + ");\n";
-            res += newMsg;
-        }
-
-        return res;
-    }
-
-    private String helper(int len, String msg) {
-
-        for (int i = 0; i<len; i++) {
-            msg += "   removeLast();\n";
-        }
-        return msg;
-    }
-
-    /*@Test
     public void testRandomRemoveLast() {
         int range = StdRandom.uniform(10);
+        Integer ansLast = null;
+        Integer ansFirst = null;
         for (int i = 0; i < range; i++) {
+            if (i == 0) {
+                ansLast = i;
+                ansFirst = i;
+            }
             double check = StdRandom.uniform();
             if (check < 0.5) {
                 stuA.addLast(i);
                 arrD.addLast(i);
+                ansLast = i;
+                // System.out.println("addLast: " + i);
             } else {
                 stuA.addFirst(i);
                 arrD.addFirst(i);
+                ansFirst = i;
+                // System.out.println("addFirst: " + i);
             }
         }
-        for (int i = 0; i < range; i++) {
-            stuA.
-        }
-        assertEquals(stuA.removeLast(), arrD.removeLast());
-    }*/
+        stuA.printDeque();
+        // System.out.println("last: " + stuA.removeLast());
+        // System.out.println("first: " + stuA.removeFirst());
+        assertEquals("removeLast() is wrong.", arrD.removeLast(), stuA.removeLast());
+        assertEquals("removeFirst() is wrong.", arrD.removeFirst(), stuA.removeFirst());
+        // System.out.println(arrD.removeFirst());
 
-    /*@Test
-    public void testRandomRemoveFirst() {
-        int range = StdRandom.uniform(10);
-        for (int i = 0; i < range; i++) {
-            double check = StdRandom.uniform();
-            if (check < 0.5) {
-                stuA.addLast(i);
-                arrD.addLast(i);
-            } else {
-                stuA.addFirst(i);
-                arrD.addFirst(i);
-            }
-        }
-        assertEquals(stuA.removeFirst(), arrD.removeFirst());
-    }*/
+    }
+
 
 
 
